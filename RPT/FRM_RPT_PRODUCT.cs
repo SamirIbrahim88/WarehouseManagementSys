d11dd37caf84ff92164d8d16412cd75f0d9fed85
SamirIbrahim88 <engabdelhady34@hotmail.com>
2017-04-09 11:04:49 +0300

    Add files via upload

---
+++ RPT/FRM_RPT_PRODUCT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WarehouseManagementSystem1.RPT
+{
+    public partial class FRM_RPT_PRODUCT : Form
+    {
+        public FRM_RPT_PRODUCT()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
