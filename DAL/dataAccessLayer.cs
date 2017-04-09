0d7f9629a50ab23b02c68db180a9fc0151d37b02
SamirIbrahim88 <engabdelhady34@hotmail.com>
2017-04-09 10:58:09 +0300

    Add files via upload

---
+++ DAL/dataAccessLayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WarehouseManagementSystem1.DAL
+{
+    class dataAccessLayer
+    {
+        SqlConnection sqlconnection;
+        public dataAccessLayer()
+        {
+            sqlconnection = new SqlConnection(@"server=DESKTOP-SM0L2DR;Initial Catalog=PRODUCT_DB;Integrated Security=True");
+
+        }
+        //method to open the connection
+        public void open()
+        {
+            if (sqlconnection.State != ConnectionState.Open)
+            {
+                sqlconnection.Open();
+            }
+        }
+        public void close()
+        {
+            if (sqlconnection.State== ConnectionState.Open)
+            {
+                sqlconnection.Close();
+            }
+        }
+        public DataTable selectdata(string stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_procedure;
+            sqlcmd.Connection = sqlconnection;
+            if (param != null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                {
+                    sqlcmd.Parameters.Add(param[i]);
+                }
+
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        public void executecommand(string stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = stored_procedure;
+            sqlcmd.Connection = sqlconnection;
+
+            if (param != null)
+            {
+                sqlcmd.Parameters.AddRange(param);
+            }
+            sqlcmd.ExecuteNonQuery();
+        }
+    }
+}
