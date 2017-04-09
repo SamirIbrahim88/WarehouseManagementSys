c887210c6cb2c67c3f55a736d62f80bafd033f3f
SamirIbrahim88 <engabdelhady34@hotmail.com>
2017-04-09 11:05:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WarehouseManagementSystem1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        public static string SALESMAN;
+        public static string PAYMAN;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new PL.FRM_BACKUP());
+            Application.Run(new PL.FRM_LOGIN());
+        }
+    }
+}
